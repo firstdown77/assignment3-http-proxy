f379c5636a5d38bb90b759b62eb5762fb0598457
Raphael Astrow <rastrow@andrew.cmu.edu>
2014-05-11 01:51:56 +0300

    IRequestHandler interface is for David.

--- src/il/technion/cs236369/proxy/HttpProxy.java
+++ src/il/technion/cs236369/proxy/HttpProxy.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import javax.net.ServerSocketFactory;
@@ -11,21 +12,13 @@ import javax.net.SocketFactory;
 
 import org.apache.http.HttpException;
 import org.apache.http.HttpHost;
-import org.apache.http.HttpRequestInterceptor;
-import org.apache.http.HttpResponseInterceptor;
-import org.apache.http.impl.DefaultBHttpClientConnection;
 import org.apache.http.impl.DefaultBHttpServerConnection;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpProcessor;
+import org.apache.http.protocol.HttpProcessorBuilder;
 import org.apache.http.protocol.HttpRequestExecutor;
 import org.apache.http.protocol.HttpService;
-import org.apache.http.protocol.ImmutableHttpProcessor;
-import org.apache.http.protocol.RequestConnControl;
-import org.apache.http.protocol.RequestContent;
-import org.apache.http.protocol.RequestExpectContinue;
-import org.apache.http.protocol.RequestTargetHost;
-import org.apache.http.protocol.RequestUserAgent;
 import org.apache.http.protocol.ResponseConnControl;
 import org.apache.http.protocol.ResponseContent;
 import org.apache.http.protocol.ResponseDate;
@@ -40,11 +33,6 @@ import com.google.inject.name.Named;
 public class HttpProxy extends AbstractHttpProxy {
 	private ServerSocket serverSocket;
 	private final int BUFSIZE = 8 * 1024;
-    private static final String HTTP_IN_CONN = "http.proxy.in-conn";
-    private static final String HTTP_OUT_CONN = "http.proxy.out-conn";
-    private static final String HTTP_CONN_KEEPALIVE = "http.proxy.conn-keepalive";
-    Socket insocket;
-    Socket outsocket;
 
 	@Inject
 	HttpProxy(SocketFactory sockFactory, ServerSocketFactory srvSockFactory,
@@ -72,83 +60,40 @@ public class HttpProxy extends AbstractHttpProxy {
 	 */
 	
 	@Override
-	//I have been looking at this tutorial: 
-	//docs.oracle.com/javase/tutorial/displayCode.html?code=http://docs.oracle.com/javase/tutorial/networking/sockets/examples/KnockKnockServer.java
 	public void bind() throws IOException {
 		serverSocket = srvSockFactory.createServerSocket(port); //This line does the bind.
 	}
 
 	@Override
 	public void start() {
-        // Set up HTTP protocol processor for incoming connections
-        final HttpProcessor inhttpproc = new ImmutableHttpProcessor(
-                new HttpRequestInterceptor[] {
-                        new RequestContent(),
-                        new RequestTargetHost(),
-                        new RequestConnControl(),
-                        new RequestUserAgent("Test/1.1"),
-                        new RequestExpectContinue(true)
-         });
-
-        // Set up HTTP protocol processor for outgoing connections
-        final HttpProcessor outhttpproc = new ImmutableHttpProcessor(
-                new HttpResponseInterceptor[] {
-                        new ResponseDate(),
-                        new ResponseServer("Test/1.1"),
-                        new ResponseContent(),
-                        new ResponseConnControl()
-        });
+        // Set up the HTTP protocol processor
+        HttpProcessor httpproc = HttpProcessorBuilder.create()
+                .add(new ResponseDate())
+                .add(new ResponseServer("Test/1.1"))
+                .add(new ResponseContent())
+                .add(new ResponseConnControl()).build();
         // Set up request handlers
         UriHttpRequestHandlerMapper reqistry = new UriHttpRequestHandlerMapper();
 		HttpHost target = new HttpHost("localhost", port);
         final HttpRequestExecutor httpexecutor = new HttpRequestExecutor();
-        reqistry.register("*", new OurRequestHandler(target, outhttpproc, httpexecutor));
+        //reqistry.register("*", new OurRequestHandler(target, httpproc, httpexecutor));
         // Set up the HTTP service
-        HttpService httpService = new HttpService(inhttpproc, reqistry);
+        HttpService httpService = new HttpService(httpproc, reqistry);
         HttpContext coreContext = new BasicHttpContext(null);
         while (true) {
            	try {
-        			Socket insocket = serverSocket.accept();
-                    DefaultBHttpServerConnection inconn = new DefaultBHttpServerConnection(BUFSIZE);
-                    System.out.println("Incoming connection from " + insocket.getInetAddress());
-                    inconn.bind(insocket);
-                    Socket outsocket = sockFactory.createSocket(target.getHostName(), target.getPort());
-                    DefaultBHttpClientConnection outconn = new DefaultBHttpClientConnection(BUFSIZE);
-        	        outconn.bind(outsocket);
-                    coreContext.setAttribute(HTTP_IN_CONN, inconn);
-					coreContext.setAttribute(HTTP_OUT_CONN, outconn);
-                    System.out.println("Outgoing connection to " + outsocket.getInetAddress());
-                    if (!inconn.isOpen()) {
-                    	System.out.println("Closing");
-                        outconn.close();
-                        break;
-                    }
-                   	httpService.handleRequest(inconn, coreContext);
-                    final Boolean keepalive = (Boolean) coreContext.getAttribute(HTTP_CONN_KEEPALIVE);
-                    if (!Boolean.TRUE.equals(keepalive)) {
-                        outconn.close();
-                        inconn.close();
-                        System.out.println("Don't keep alive.");
-                        break;
-                    }
+        			Socket socket = serverSocket.accept();
+                    DefaultBHttpServerConnection conn = new DefaultBHttpServerConnection(BUFSIZE);
+                    System.out.println("Incoming connection from " + socket.getInetAddress());
+                    conn.bind(socket);
+                    httpService.handleRequest(conn, coreContext);
            	} catch (IOException e) {
 				e.printStackTrace();
            	} catch (HttpException e) {
 				e.printStackTrace();
 			}
             finally {
-            	/*
-            	try {
-            		if (!insocket.isClosed()) {
-    					insocket.close();
-            		}
-            		if (!outsocket.isClosed()) {
-    	            	outsocket.close();
-            		}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				*/
+            	//Close anything that needs to be closed.
             }
         }
     }
