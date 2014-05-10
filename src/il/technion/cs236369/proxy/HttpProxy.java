2f81ff72e9e57153b10b5641b83acfe58169e4ba
Raphael Astrow <rastrow@andrew.cmu.edu>
2014-05-10 19:13:41 +0300

    Still stuck on httpexecutor.execute

--- src/il/technion/cs236369/proxy/HttpProxy.java
+++ src/il/technion/cs236369/proxy/HttpProxy.java
@@ -2,23 +2,32 @@ package il.technion.cs236369.proxy;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Properties;
 
 import javax.net.ServerSocketFactory;
 import javax.net.SocketFactory;
 
-import org.apache.http.HttpConnectionFactory;
 import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.HttpResponseInterceptor;
+import org.apache.http.impl.DefaultBHttpClientConnection;
 import org.apache.http.impl.DefaultBHttpServerConnection;
-import org.apache.http.impl.DefaultBHttpServerConnectionFactory;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpProcessor;
-import org.apache.http.protocol.HttpProcessorBuilder;
-import org.apache.http.protocol.HttpRequestHandler;
+import org.apache.http.protocol.HttpRequestExecutor;
 import org.apache.http.protocol.HttpService;
+import org.apache.http.protocol.ImmutableHttpProcessor;
+import org.apache.http.protocol.RequestConnControl;
+import org.apache.http.protocol.RequestContent;
+import org.apache.http.protocol.RequestExpectContinue;
+import org.apache.http.protocol.RequestTargetHost;
+import org.apache.http.protocol.RequestUserAgent;
 import org.apache.http.protocol.ResponseConnControl;
 import org.apache.http.protocol.ResponseContent;
 import org.apache.http.protocol.ResponseDate;
@@ -31,8 +40,14 @@ import com.google.inject.Injector;
 import com.google.inject.name.Named;
 
 public class HttpProxy extends AbstractHttpProxy {
-	ServerSocket serverSocket;
-	DefaultBHttpServerConnection conn;
+	private ServerSocket serverSocket;
+	private final int BUFSIZE = 8 * 1024;
+    private static final String HTTP_IN_CONN = "http.proxy.in-conn";
+    private static final String HTTP_OUT_CONN = "http.proxy.out-conn";
+    private static final String HTTP_CONN_KEEPALIVE = "http.proxy.conn-keepalive";
+    Socket insocket;
+    Socket outsocket;
+
 	@Inject
 	HttpProxy(SocketFactory sockFactory, ServerSocketFactory srvSockFactory,
 
@@ -66,36 +81,74 @@ public class HttpProxy extends AbstractHttpProxy {
 	}
 
 	@Override
-	/**
-	 * Current version copied from: http://cs.au.dk/~amoeller/WWW/javaweb/server.html
-	 */
 	public void start() {
-        // Set up the HTTP protocol processor
-        HttpProcessor httpproc = HttpProcessorBuilder.create()
-                .add(new ResponseDate())
-                .add(new ResponseServer("Test/1.1"))
-                .add(new ResponseContent())
-                .add(new ResponseConnControl()).build();
+        // Set up HTTP protocol processor for incoming connections
+        final HttpProcessor inhttpproc = new ImmutableHttpProcessor(
+                new HttpRequestInterceptor[] {
+                        new RequestContent(),
+                        new RequestTargetHost(),
+                        new RequestConnControl(),
+                        new RequestUserAgent("Test/1.1"),
+                        new RequestExpectContinue(true)
+         });
+
+        // Set up HTTP protocol processor for outgoing connections
+        final HttpProcessor outhttpproc = new ImmutableHttpProcessor(
+                new HttpResponseInterceptor[] {
+                        new ResponseDate(),
+                        new ResponseServer("Test/1.1"),
+                        new ResponseContent(),
+                        new ResponseConnControl()
+        });
         // Set up request handlers
         UriHttpRequestHandlerMapper reqistry = new UriHttpRequestHandlerMapper();
-        HttpRequestHandler handler = new OurRequestHandler();
-        reqistry.register("*", handler);
+		HttpHost target = new HttpHost("localhost", port);
+        final HttpRequestExecutor httpexecutor = new HttpRequestExecutor();
+        reqistry.register("*", new OurRequestHandler(target, outhttpproc, httpexecutor));
         // Set up the HTTP service
-        HttpService httpService = new HttpService(httpproc, reqistry);
-    	HttpConnectionFactory<DefaultBHttpServerConnection> connFactory = DefaultBHttpServerConnectionFactory.INSTANCE;
-        HttpContext coreContext = new BasicHttpContext();
+        HttpService httpService = new HttpService(inhttpproc, reqistry);
+        HttpContext coreContext = new BasicHttpContext(null);
            while (true) {
            	try {
-        			Socket socket = serverSocket.accept();
-                    DefaultBHttpServerConnection conn = connFactory.createConnection(socket);
-                   	httpService.handleRequest(conn, coreContext);
+        			Socket insocket = serverSocket.accept();
+                    DefaultBHttpServerConnection inconn = new DefaultBHttpServerConnection(BUFSIZE);
+                    System.out.println("Incoming connection from " + insocket.getInetAddress());
+                    inconn.bind(insocket);
+                    Socket outsocket = sockFactory.createSocket(target.getHostName(), target.getPort());
+                    DefaultBHttpClientConnection outconn = new DefaultBHttpClientConnection(BUFSIZE);
+        	        outconn.bind(outsocket);
+                    coreContext.setAttribute(HTTP_IN_CONN, inconn);
+					coreContext.setAttribute(HTTP_OUT_CONN, outconn);
+                    System.out.println("Outgoing connection to " + outsocket.getInetAddress());
+                    if (!inconn.isOpen()) {
+                    	System.out.println("Closing");
+                        outconn.close();
+                        break;
+                    }
+                   	httpService.handleRequest(inconn, coreContext);
+                    final Boolean keepalive = (Boolean) coreContext.getAttribute(HTTP_CONN_KEEPALIVE);
+                    if (!Boolean.TRUE.equals(keepalive)) {
+                        outconn.close();
+                        inconn.close();
+                        System.out.println("Don't keep alive.");
+                        break;
+                    }
            	} catch (IOException e) {
 				e.printStackTrace();
            	} catch (HttpException e) {
 				e.printStackTrace();
 			}
             finally {
-            	//TODO decide if finally is necessary
+            	try {
+            		if (!insocket.isClosed()) {
+    					insocket.close();
+            		}
+            		if (!outsocket.isClosed()) {
+    	            	outsocket.close();
+            		}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
             }
         }
     }
--- src/il/technion/cs236369/proxy/OurRequestHandler.java
+++ src/il/technion/cs236369/proxy/OurRequestHandler.java
@@ -1,26 +1,44 @@
 package il.technion.cs236369.proxy;
 
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.util.Locale;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.ConnectionReuseStrategy;
+import org.apache.http.HttpClientConnection;
 import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.MethodNotSupportedException;
-import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.DefaultConnectionReuseStrategy;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpCoreContext;
+import org.apache.http.protocol.HttpProcessor;
+import org.apache.http.protocol.HttpRequestExecutor;
 import org.apache.http.protocol.HttpRequestHandler;
-import org.apache.http.util.EntityUtils;
 
 public class OurRequestHandler implements HttpRequestHandler {
+    private HttpHost target;
+    private HttpProcessor httpproc;
+    private HttpRequestExecutor httpexecutor;
+    private ConnectionReuseStrategy connStrategy;
+    
+    //private static final String HTTP_IN_CONN = "http.proxy.in-conn";
+    private static final String HTTP_OUT_CONN = "http.proxy.out-conn";
+    private static final String HTTP_CONN_KEEPALIVE = "http.proxy.conn-keepalive";
 
+    public OurRequestHandler(HttpHost target, HttpProcessor httpproc, HttpRequestExecutor httpexecutor) {
+        this.target = target;
+        this.httpproc = httpproc;
+        this.httpexecutor = httpexecutor;
+        this.connStrategy = DefaultConnectionReuseStrategy.INSTANCE;
+    }
+    
+    
+    
 	@Override
 	public void handle(HttpRequest request, HttpResponse response, HttpContext context)
 			throws HttpException, IOException {
+		/*
 		System.out.println("Handling");
         String method = request.getRequestLine().getMethod().toUpperCase(Locale.ENGLISH);
         if (!method.equals("GET") && !method.equals("HEAD") && !method.equals("POST")) {
@@ -33,14 +51,46 @@ public class OurRequestHandler implements HttpRequestHandler {
             byte[] entityContent = EntityUtils.toByteArray(entity);
             System.out.println("Incoming entity content (bytes): " + entityContent.length);
         }
-        response.setStatusCode(HttpStatus.SC_OK);
-        StringEntity entity = new StringEntity(decodedTarget);
-        response.setEntity(entity);
+        */
+        final HttpClientConnection conn = (HttpClientConnection) context.getAttribute(
+                HTTP_OUT_CONN);
+
+        context.setAttribute(HttpCoreContext.HTTP_CONNECTION, conn);
+        context.setAttribute(HttpCoreContext.HTTP_TARGET_HOST, this.target);
+        
+        System.out.println(">> Request URI: " + request.getRequestLine().getUri());
 
         
-		//System.out.println("arg0: " + request);
-		//System.out.println(response);
-		System.out.println(context);
-	}
+        // Remove hop-by-hop headers
+        request.removeHeaders(HTTP.CONTENT_LEN);
+        request.removeHeaders(HTTP.TRANSFER_ENCODING);
+        request.removeHeaders(HTTP.CONN_DIRECTIVE);
+        request.removeHeaders("Keep-Alive");
+        request.removeHeaders("Proxy-Authenticate");
+        request.removeHeaders("TE");
+        request.removeHeaders("Trailers");
+        request.removeHeaders("Upgrade");
+        
+        this.httpexecutor.preProcess(request, this.httpproc, context);
+        HttpResponse targetResponse = this.httpexecutor.execute(request, conn, context);
+        this.httpexecutor.postProcess(response, this.httpproc, context);
+        
+        // Remove hop-by-hop headers
+        targetResponse.removeHeaders(HTTP.CONTENT_LEN);
+        targetResponse.removeHeaders(HTTP.TRANSFER_ENCODING);
+        targetResponse.removeHeaders(HTTP.CONN_DIRECTIVE);
+        targetResponse.removeHeaders("Keep-Alive");
+        targetResponse.removeHeaders("TE");
+        targetResponse.removeHeaders("Trailers");
+        targetResponse.removeHeaders("Upgrade");
+        
+        response.setStatusLine(targetResponse.getStatusLine());
+        response.setHeaders(targetResponse.getAllHeaders());
+        response.setEntity(targetResponse.getEntity());
+        
+        System.out.println("<< Response: " + response.getStatusLine());
 
+        final boolean keepalive = this.connStrategy.keepAlive(response, context);
+        context.setAttribute(HTTP_CONN_KEEPALIVE, new Boolean(keepalive));
+	}
 }
