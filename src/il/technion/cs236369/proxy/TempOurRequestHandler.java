b3df5827993707ffdec209308e55c3685b7e076e
Raphael Astrow <rastrow@andrew.cmu.edu>
2014-05-12 01:46:19 +0300

    Did some database coding

--- src/il/technion/cs236369/proxy/TempOurRequestHandler.java
+++ src/il/technion/cs236369/proxy/TempOurRequestHandler.java
@@ -1,7 +1,9 @@
 package il.technion.cs236369.proxy;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Locale;
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
@@ -13,50 +15,71 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpRequestHandler;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 public class TempOurRequestHandler implements HttpRequestHandler {
-    
     CloseableHttpClient client = HttpClients.createDefault();
+	private Cache theCache;
+    
+    public TempOurRequestHandler() {
+        Injector inj = Guice.createInjector(new HttpProxyModule());
+		theCache = inj.getInstance(Cache.class);
+    }
     
 	@Override
 	public void handle(HttpRequest request, HttpResponse response, HttpContext context)
 			throws HttpException, IOException {
-		
 		String method = request.getRequestLine().getMethod().toUpperCase(Locale.ENGLISH);
         if (!method.equals("GET") && !method.equals("HEAD")) {
             throw new MethodNotSupportedException(method + " method not supported");
         }
         String target = request.getRequestLine().getUri();
         System.out.println(target);
-        /*
-        HttpEntity theEntity = null;
-        if (request instanceof HttpEntityEnclosingRequest) {
-            theEntity = ((HttpEntityEnclosingRequest) request).getEntity();
-            byte[] entityContent = EntityUtils.toByteArray(theEntity);
-            System.out.println("Incoming entity content (bytes): " + entityContent.length);
+        HttpGet requestToClient = new HttpGet(target);
+        CloseableHttpResponse responseFromClient = client.execute(requestToClient);
+        HttpEntity theEntity = responseFromClient.getEntity();
+        Header[] h = responseFromClient.getHeaders("Cache-Control");
+        boolean nocache = false;
+        boolean nostore = false;
+        for (int i = 0; i < h.length; i++) {
+        	if (h[i].getValue().equals("no-cache")) {
+        		nocache = true;
+        	}
+        	if (h[i].getValue().equals("no-store")) {
+        		nostore = true;
+        	}
         }
-        */
-        HttpGet request2 = new HttpGet(target);
-        CloseableHttpResponse response2 = client.execute(request2);
-        HttpEntity theEntity = response2.getEntity();
-        /*InputStream theReader = theEntity.getContent();
-        //Get the response
-        
-        
-        BufferedReader rd = new BufferedReader
-          (new InputStreamReader(theReader));
-            
-        String line = "";
-        while ((line = rd.readLine()) != null) {
-        	System.out.println(line);
-        } */
-        
-        //byte[] responseBody = method2.getResponseBody();
-        
+		if (nocache) {
+			//store in cache if necessary but do not serve from cache.  Serve from fresh response.
+			try {
+				theCache.insert(target, responseFromClient);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	    if (!nocache && !nostore && responseFromClient.toString().length() < 65535
+	    		&& method.equals("GET") && responseFromClient.getStatusLine().getStatusCode() == 200
+	    		&& responseFromClient.containsHeader("Last-Modified")) {
+	    	//may serve from cache if the cache contains the resource.
+	    	HttpResponse cachedResponse;
+			try {
+				cachedResponse = theCache.search(target);
+		    	if (cachedResponse != null) {
+		    		System.out.println("Serving from cache");
+		    		theEntity = cachedResponse.getEntity();
+		    	}
+		    	else {
+		    		theCache.insert(target, cachedResponse);
+		    	}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	    }
         response.setStatusCode(200);
         response.setHeaders(request.getAllHeaders());
         response.setEntity(theEntity);
-        
         System.out.println("<< Response: " + response.getStatusLine());
 	}
 }
